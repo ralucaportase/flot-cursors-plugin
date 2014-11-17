90fb8320ebeca8c7e7f0d8e384690f62f5a71367
Ciprian Ceteras <cipix2000@gmail.com>
2014-11-17 19:22:45 +0200

    it's intersections not interactions

--- spec/cursors_intersections_Spec.js
+++ spec/cursors_intersections_Spec.js
@@ -1,7 +1,7 @@
 /* global $, describe, it, xit, after, beforeEach, afterEach, expect, jasmine */
 /* jshint browser: true*/
 
-describe("Cursors interactions", function () {
+describe("Cursors intersections", function () {
     var sampledata = [[0, 1], [1, 1.1], [2, 1.2]];
     var sampledata2 = [[0, 2], [1, 2.1], [2, 2.2]];
 
